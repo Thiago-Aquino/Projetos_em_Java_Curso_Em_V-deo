6dc4c55aada0c963a5530091e290002c79a74d86
Thiago-Aquino <105040945+Thiago-Aquino@users.noreply.github.com>
2024-01-24 20:11:38 -0300

    Add files via upload

---
+++ OperadoradoresAritmeticos/src/operadoradoresaritmeticos/OperadoradoresAritmeticos.java
@@ -0,0 +1,32 @@
+package operadoradoresaritmeticos;
+public class OperadoradoresAritmeticos {
+    public static void main(String[] args) {
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A media : " + m);
+        */
+        /*
+        int numero = 5;
+        int valor = 4 + numero++;
+        System.out.println(valor);
+        System.out.println(numero);
+        */
+        /*
+        int x = 4;
+        x +=2; // x = x + 2
+        System.out.println(x);
+        */
+        /*
+        float v = 8.6f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);
+        */
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+    }
+    
+}
